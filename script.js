28480bf2353ca03393d973923aea8e3f8fccaad4
Pramod Koushik T R <pramodkoushiktr@gmail.com>
2026-04-10 15:45:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 function toggleTheme(){
   const current = localStorage.getItem('theme');
+  const icon = document.getElementById('themeIcon');
 
   if(current === 'light'){
     document.body.classList.remove('light');
     localStorage.setItem('theme','dark');
+    icon.innerHTML = "🌙";
   } else{
     document.body.classList.add('light');
     localStorage.setItem('theme','light');
+    icon.innerHTML = "☀️";
   }
 }
 
 window.onload = () => {
   const saved = localStorage.getItem('theme');
+  const icon = document.getElementById('themeIcon');
+
   if(saved === 'light'){
     document.body.classList.add('light');
+    if(icon) icon.innerHTML = "☀️";
+  } else{
+    if(icon) icon.innerHTML = "🌙";
   }
 };
