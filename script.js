2a501719efb6c863d4ec5fb6be46f5abbd1cd27c
Pramod Koushik T R <pramodkoushiktr@gmail.com>
2026-04-19 19:03:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,18 +46,3 @@ window.onload = function () {
   const defaultLang = savedLang || "en";
   setLanguage(defaultLang);
 };
-
-function doPost(e) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  
-  var data = JSON.parse(e.postData.contents);
-
-  sheet.appendRow([
-    data.name,
-    data.email,
-    data.message,
-    new Date() // auto timestamp
-  ]);
-
-  return ContentService.createTextOutput("Success");
-}
