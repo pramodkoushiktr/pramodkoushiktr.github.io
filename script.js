ba8ace07f4e14dd9d3059fb4921fafd12f5b7191
Pramod Koushik T R <pramodkoushiktr@gmail.com>
2026-06-29 21:07:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,30 @@ window.onload = function () {
   setLanguage(defaultLang);
 };
 
+
+
+
+
+async function loadBooks(year, minRating) {
+
+    const url = "https://script.google.com/macros/s/AKfycbztcRGkNZt2sVgnq6CnmrvpGZrzbNdNrb68rkHuTrpOVG2ZGZxiZxIXvjlqHW3e19PwHg/exec";
+
+    const response = await fetch(url);
+    const books = await response.json();
+
+    const container = document.getElementById("book-list");
+    container.innerHTML = "";
+
+    books
+        .filter(book =>
+            book.read_year == year &&
+            book.rating >= minRating
+        )
+        .forEach(book => {
+
+            const div = document.createElement("div");
+            div.textContent = `${book.book_name} | ${book.author}`;
+            container.appendChild(div);
+
+        });
+}
